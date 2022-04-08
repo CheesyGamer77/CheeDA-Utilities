c7c3bee291136957229d18d11d2cfd8a367eeb66
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-04-07 20:49:26 -0400

    Add SlashCommand

---
+++ src/main/java/pw/cheesygamer77/cheedautilities/commands/SlashCommand.java
@@ -0,0 +1,45 @@
+package pw.cheesygamer77.cheedautilities.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.commands.Command.Type;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public abstract class SlashCommand implements Command<SlashCommandData, SlashCommandInteractionEvent> {
+    private final List<Predicate<SlashCommandInteractionEvent>> predicates = new ArrayList<>();
+    private final SlashCommandData data;
+
+    public SlashCommand(@NotNull SlashCommandData data) {
+        this.data = data;
+    }
+
+    public SlashCommand(@NotNull SlashCommandData data, Predicate<SlashCommandInteractionEvent> predicate) {
+        this(data);
+        addPredicate(predicate);
+    }
+
+    @Override
+    public @NotNull Type getType() {
+        return Type.SLASH;
+    }
+
+    @Override
+    public @NotNull SlashCommandData getData() {
+        return this.data;
+    }
+
+    @Override
+    public @NotNull List<Predicate<SlashCommandInteractionEvent>> getPredicates() {
+        return predicates;
+    }
+
+    @Override
+    public @NotNull Command<SlashCommandData, SlashCommandInteractionEvent> addPredicate(@NotNull Predicate<SlashCommandInteractionEvent> predicate) {
+        this.predicates.add(predicate);
+        return this;
+    }
+}
