77c3b580882745a13660bbe1d47e8c061c7bd955
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-04-08 16:15:08 -0400

    Add Command#canInvoke, remove return type of Command#addPredicate, change D type param

--- src/main/java/pw/cheesygamer77/cheedautilities/commands/Command.java
+++ src/main/java/pw/cheesygamer77/cheedautilities/commands/Command.java
@@ -3,8 +3,7 @@ package pw.cheesygamer77.cheedautilities.commands;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.Command.Type;
-
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.utils.data.SerializableData;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -15,7 +14,7 @@ import java.util.function.Predicate;
  * @param <D> The command data type
  * @param <E> The invoking event type
  */
-public interface Command<D extends CommandData, E extends Event> {
+public interface Command<D extends SerializableData, E extends Event> {
     /**
      * Returns the name of this command
      * @return The command name
@@ -36,10 +35,9 @@ public interface Command<D extends CommandData, E extends Event> {
 
     /**
      * Adds a predicate to this command
-     * @param predicate The predicate
-     * @return The current command
+     * @param predicate The predicate to add
      */
-    @NotNull Command<D, E> addPredicate(@NotNull Predicate<SlashCommandInteractionEvent> predicate);
+    void addPredicate(@NotNull Predicate<SlashCommandInteractionEvent> predicate);
 
     /**
      * Returns this command's data
@@ -56,17 +54,27 @@ public interface Command<D extends CommandData, E extends Event> {
     }
 
     /**
+     * Returns whether this command can be invoked given the current context
+     * @param event The event context
+     * @return Whether the command may be invoked or not
+     */
+    default boolean canInvoke(@NotNull E event) {
+        for(Predicate<E> predicate : getPredicates())
+            // TODO: Allow for custom predicate failure handling
+            if(!predicate.test(event))
+                return false;
+
+        return true;
+    }
+
+    /**
      * Calls this command
      *
      * This method runs any predicates and invokes the command if all predicates pass
      * @param event The event that called this command
      */
     default void call(@NotNull E event) {
-        for(Predicate<E> predicate : getPredicates())
-            // TODO: Allow for custom predicate failure handling
-            if(!predicate.test(event))
-                return;
-
-        invoke(event);
+        if(canInvoke(event))
+            invoke(event);
     }
 }
