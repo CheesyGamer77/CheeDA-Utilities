dd295ca227a7669c1ff17cbf4459650631fb79dd
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-04-11 18:06:53 -0400

    Add SubcommandGroup#call and SubcommandGroup#invoke

--- src/main/java/pw/cheesygamer77/cheedautilities/commands/slash/SubcommandGroup.java
+++ src/main/java/pw/cheesygamer77/cheedautilities/commands/slash/SubcommandGroup.java
@@ -6,11 +6,13 @@ import org.jetbrains.annotations.NotNull;
 import pw.cheesygamer77.cheedautilities.commands.slash.internal.SlashCommandInvokable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.function.Predicate;
 
 public abstract class SubcommandGroup implements SlashCommandInvokable<SubcommandGroupData> {
     private final List<Predicate<SlashCommandInteractionEvent>> predicates = new ArrayList<>();
+    private final HashMap<String, Subcommand> subcommandMapping = new HashMap<>();  // subcommand name to subcommand instance
 
     private final SubcommandGroupData data;
 
@@ -29,12 +31,15 @@ public abstract class SubcommandGroup implements SlashCommandInvokable<Subcomman
     }
 
     @Override
-    public void addPredicate(@NotNull Predicate<SlashCommandInteractionEvent> predicate) {
-        this.predicates.add(predicate);
+    public void call(@NotNull SlashCommandInteractionEvent event) {
+        if(event.getSubcommandName() == null) return;
+
+        Subcommand subcommand = subcommandMapping.get(event.getSubcommandName());
+        if(subcommand != null) subcommand.call(event);
     }
 
     @Override
-    public @NotNull List<Predicate<SlashCommandInteractionEvent>> getPredicates() {
-        return predicates;
+    public final void invoke(@NotNull SlashCommandInteractionEvent event) {
+        throw new IllegalStateException("Subcommand Groups cannot be invoked as commands");
     }
 }
